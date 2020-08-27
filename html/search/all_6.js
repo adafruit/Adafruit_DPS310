6b32113142660bf7ede57f046e81dd6180cd19b7
Doxygen CI <travis@travis-ci.org>
2020-08-27 17:09:17 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['readaltitude',['readAltitude',['../class_adafruit___d_p_s310.html#a4b06532fca6e675b530997364db00d30',1,'Adafruit_DPS310']]],
   ['reset',['reset',['../class_adafruit___d_p_s310.html#a68bb7c0ae0c933c6ee67461a622231fc',1,'Adafruit_DPS310']]]
 ];
